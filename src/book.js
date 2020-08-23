b15a03eecec654707ce56af75cdee147d545b4ea
Joe Lopez <lopez.4.joseph@gmail.com>
2020-08-22 19:21:18 -0600

    Create functions that pass unit testing

--- src/book.js
+++ src/book.js
@@ -1,12 +1,45 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return bookIdea = "The " + title;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  var ghoulCharacter = {
+    name: name,
+    age: age, 
+    pronouns: pronouns,
+  };
+  return ghoulCharacter;
+}
+
+function saveReview(reviewQuote, reviews) {
+  if (reviews[i] != reviewQuote) {
+    reviews.push(reviewQuote)
+  }
+}
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20;
+}
+
+function writeBook(title, mainCharacter, genre) {
+  var book = {
+    title,
+    mainCharacter,
+    pageCount: calculatePageCount(title),
+    genre,
+  }
+  return book;
+}
+
+function editBook(book) {
+  return book.pageCount = book.pageCount * .75;
 }
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
-  // writeBook: writeBook,
-  // editBook: editBook
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
+  writeBook: writeBook,
+  editBook: editBook
 }
