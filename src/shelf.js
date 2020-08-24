4acc8949a407ea7a9f000229ea7f26b282757b90
Joe Lopez <lopez.4.joseph@gmail.com>
2020-08-23 20:11:36 -0600

    Create functions to satisfy the corresponding unit tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,8 +1,33 @@
+function shelfBook(title, shelfCategory) {
+  if (shelfCategory.length < 3) {
+    shelfCategory.unshift(title);
+  }
+}
 
+function unshelfBook(title, shelfCategory) {
+  for (i = 0; i < shelfCategory.length; i++) {
+    if (title === shelfCategory[i].title) {
+      shelfCategory.splice(i, 1);  
+    }
+  }
+}
+
+function listTitles(shelfCategory) {
+  var shelfItems = [];
+  for (i = 0; i < shelfCategory.length; i++) {
+    shelfItems.push(shelfCategory[i].title)
+  } 
+  var newList = shelfItems.join(", ")
+  return newList;   
+}
+
+function searchShelf(shelfCategory, title) {
+  return listTitles(shelfCategory).includes(title) > 0;
+}
 
 module.exports = {
-  // shelfBook: shelfBook,
-  // unshelfBook: unshelfBook,
-  // listTitles: listTitles,
-  // searchShelf: searchShelf
+  shelfBook: shelfBook,
+  unshelfBook: unshelfBook,
+  listTitles: listTitles,
+  searchShelf: searchShelf
 };
